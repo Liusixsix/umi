020193d432928a90ba9a6e57e69285316f2d3f6e
liuyan <450995596@qq.com>
2020-07-17 21:06:58 +0800

    sku

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,66 @@
-import React, { useEffect } from 'react';
-import { Chart } from '@antv/g2';
+import React, { useEffect,useState,useMemo,} from 'react';
+import AdjoinMatrix from './utils'
+import SpecAdjoinMatrix from './utils2'
+import './index.less'
+let specList = [
+  { title: "颜色", list: ["红色", "紫色"] },
+  { title: "套餐", list: ["套餐一", "套餐二"] },
+  { title: "内存", list: ["64G", "128G", "256G"] },
+];
 
-const Dashboard = () => {
+let specCombinationList= [
+  { id: "1", specs: ["紫色", "套餐一", "64G"] },
+  { id: "2", specs: ["紫色", "套餐一", "128G"] },
+  { id: "3", specs: ["紫色", "套餐二", "128G"] },
+  { id: "4", specs: ["红色", "套餐二", "256G"] }
+]
+const classNames = require("classnames");
+const Spec: React.FC = () => {
+  
+  // 已选择的规格，长度为规格列表的长度
+  const [specsS, setSpecsS] = useState(Array(specList.length).fill(""));
+  // 创建一个规格矩阵
+  const specAdjoinMatrix = useMemo(() => new SpecAdjoinMatrix(specList, specCombinationList), [specList, specCombinationList]);
+  // 获得可选项表
+  const optionSpecs = specAdjoinMatrix.getSpecscOptions(specsS);
 
+  const handleClick = function(bool: boolean, text: string, index: number) {
+    // 排除可选规格里面没有的规格
+    if (specsS[index] !== text && !bool) return;
+    // 根据text判断是否已经被选中了
+    specsS[index] = specsS[index] === text ? "" : text;
+    setSpecsS(specsS.slice());
+  };
 
   return (
-    <>
-      <div id="container" style={{  height: 400 }}>
-          这是首页
-      </div>
-    </>
+    <div>
+      {specList.map(({ title, list }, index) => (
+        <div key={index}>
+          <p className="title">{title}</p>
+          <div className="specBox">
+            {list.map((value:any, i:any) => {
+              const isOption = optionSpecs.includes(value); // 当前规格是否可选
+              const isActive = specsS.includes(value); // 当前规格是否被选
+              return (
+                <span
+                  key={i}
+                  className={classNames('item',{
+                    specOption: isOption,
+                    specAction: isActive,
+                    specDisabled: !isOption,
+                  })}
+                  onClick={() => handleClick(isOption, value, index)}
+                >
+                  {value}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
-export default Dashboard;
+
+export default Spec;
+
---
+++ src/pages/dashboard/utils2.ts
@@ -0,0 +1,72 @@
+/*
+ * @Description:  
+ * @Author: liu yan
+ * @Date: 2020-07-17 20:34:56
+ * @LastEditTime: 2020-07-17 20:52:35
+ */ 
+import AdjoinMatrix from "./utils";
+import { AdjoinType } from "./utils";
+
+
+export default class SpecAdjoinMatrix extends AdjoinMatrix {
+  specList: any
+  specCombinationList:any
+
+  constructor(specList: any, specCombinationList: any) {
+    super(specList.reduce((total: AdjoinType, current:any) => [...total, ...current.list], []));
+    this.specList = specList;
+    this.specCombinationList = specCombinationList;
+    // 根据可选规格列表矩阵创建
+    this.initSpec();
+    // 同级顶点创建
+    this.initSameLevel();
+  }
+
+  /**
+   * 根据可选规格组合填写邻接矩阵的值
+   */
+  initSpec() {
+    this.specCombinationList.forEach((item:any) => {
+      this.fillInSpec(item.specs);
+    });
+  }
+  // 填写同级点
+  initSameLevel() {
+    // 获得初始所有可选项
+    const specsOption = this.getCollection(this.vertex);
+    this.specList.forEach((item:any) => {
+      const params: AdjoinType = [];
+      // 获取同级别顶点
+      item.list.forEach((value:any) => {
+        if (specsOption.includes(value)) params.push(value);
+      });
+      // 同级点位创建
+      this.fillInSpec(params);
+    });
+  }
+  /*
+   * 传入顶点数组，查询出可选规格
+   * @param params
+   */
+  getSpecscOptions(params: AdjoinType) {
+    let specOptionCanchoose: AdjoinType = [];
+    if (params.some(Boolean)) {
+      // 过滤一下选项
+      specOptionCanchoose = this.getUnions(params.filter(Boolean));
+    } else {
+      // 所有可选项
+      specOptionCanchoose = this.getCollection(this.vertex);
+    }
+    return specOptionCanchoose;
+  }
+
+  /*
+   * @params
+   * 填写邻接矩阵的值
+   */
+  fillInSpec(params: AdjoinType) {
+    params.forEach((param) => {
+      this.setAdjoinVertexs(param, params);
+    });
+  }
+}
